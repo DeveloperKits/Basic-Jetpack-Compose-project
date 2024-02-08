62631bce82d6f503f3f51286743fff359daeab11
Akash Das <akashdas.dev@gmail.com>
2024-02-08 15:47:22 +0600

    meditation ui updated

---
+++ app/src/main/java/net/congressia/apb/first_project/meditation/Feature.kt
@@ -0,0 +1,8 @@
+package net.congressia.apb.first_project.meditation
+
+import androidx.annotation.DrawableRes
+
+data class Feature(
+    val tittle: String,
+    @DrawableRes val iconId: Int
+)
--- app/src/main/java/net/congressia/apb/first_project/meditation/HomePage.kt
+++ app/src/main/java/net/congressia/apb/first_project/meditation/HomePage.kt
@@ -1,23 +1,39 @@
 package net.congressia.apb.first_project.meditation
 
+import android.view.RoundedCorner
+import android.view.WindowInsets
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.magnifier
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
@@ -29,12 +45,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import net.congressia.apb.first_project.R
 import net.congressia.apb.first_project.ui.theme.ButtonBlue
 import net.congressia.apb.first_project.ui.theme.DarkerButtonBlue
 import net.congressia.apb.first_project.ui.theme.DeepBlue
+import net.congressia.apb.first_project.ui.theme.Pink40
+import net.congressia.apb.first_project.ui.theme.Pink80
+import net.congressia.apb.first_project.ui.theme.Purple80
+import net.congressia.apb.first_project.ui.theme.PurpleGrey80
 import net.congressia.apb.first_project.ui.theme.TextWhite
 
 @Composable
@@ -49,8 +72,48 @@ fun HomePage() {
             GreetingSection()
             Spacer(modifier = Modifier.height(30.dp))
             ChipSection(chips = listOf("Sweet sleep", "Insomnia", "Depression", "Happy"))
+            Spacer(modifier = Modifier.height(20.dp))
+            CurrentMeditation()
+            Spacer(modifier = Modifier.height(20.dp))
+            FeatureSection(feature = listOf(
+                Feature(
+                    tittle = "Hello Akash What up brother?",
+                    iconId = androidx.core.R.drawable.ic_call_answer_video
+                ),
+                Feature(
+                    tittle = "Call Cancel",
+                    iconId = androidx.core.R.drawable.ic_call_decline
+                ),
+                Feature(
+                    tittle = "Video Call",
+                    iconId = androidx.core.R.drawable.ic_call_answer_low
+                ),
+                Feature(
+                    tittle = "Video Call",
+                    iconId = androidx.core.R.drawable.ic_call_answer_video
+                ),
+                Feature(
+                    tittle = "Hello Akash What up brother?",
+                    iconId = androidx.core.R.drawable.ic_call_answer_video
+                ),
+                Feature(
+                    tittle = "Call Cancel",
+                    iconId = androidx.core.R.drawable.ic_call_decline
+                ),
+                Feature(
+                    tittle = "Video Call",
+                    iconId = androidx.core.R.drawable.ic_call_answer_video
+                )
+            ))
+
         }
     }
+}
+
+@Composable
+fun BottomAppBarDefaults() {
+
+
 }
 
 @Composable
@@ -118,5 +181,115 @@ fun ChipSection(
                 Text(text = chips[it], color = TextWhite)
             }
         }
+    }
+}
+
+@Composable
+fun CurrentMeditation() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .clip(RoundedCornerShape(10.dp))
+            .background(color = ButtonBlue)
+            .padding(15.dp)
+            .fillMaxWidth()
+
+    ) {
+        Column {
+            Text(
+                text = "Daily Thought",
+                color = TextWhite,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = "Meditation • 3-10 min",
+                color = TextWhite,
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+
+        Box (
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .background(color = Purple80)
+                .padding(10.dp)
+        ){
+            Icon(
+                painter = painterResource(id = androidx.core.R.drawable.ic_call_answer),
+                contentDescription = null,
+                tint = Color.White
+            )
+        }
+    }
+}
+
+@Composable
+fun FeatureSection(feature: List<Feature>) {
+    Text(
+        text = "Feature",
+        style = MaterialTheme.typography.titleMedium,
+        color = TextWhite
+    )
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(bottom = 100.dp),
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(top = 10.dp, bottom = 100.dp)
+    ){
+        items(feature.size){
+            FeatureItem(feature = feature[it])
+        }
+    }
+}
+
+@Composable
+fun FeatureItem(
+    feature: Feature
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 5.dp, end = 5.dp, bottom = 10.dp)
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(10.dp))
+            .background(color = Color.DarkGray)
+            .padding(15.dp)
+
+    ) {
+        Text(
+            text = feature.tittle,
+            style = MaterialTheme.typography.titleMedium,
+            color = TextWhite,
+            lineHeight = 24.sp,
+            modifier = Modifier.align(Alignment.TopStart)
+        )
+
+        Icon(
+            painter = painterResource(id = feature.iconId),
+            contentDescription = null,
+            tint = Color.White,
+            modifier = Modifier.align(Alignment.BottomStart)
+        )
+
+        Text(
+            text = "Start",
+            style = MaterialTheme.typography.bodyMedium,
+            color = TextWhite,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier
+                .clickable {
+
+                }
+                .align(Alignment.BottomEnd)
+                .clip(RoundedCornerShape(6.dp))
+                .background(color = Pink80)
+                .padding(vertical = 6.dp, horizontal = 10.dp)
+        )
     }
 }
